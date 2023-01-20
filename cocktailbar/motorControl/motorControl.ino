83fd62fc8ad00c0a3176d0f1d36ad163ddab20cd
DeVDavino <doubled.rosaria@gmail.com>
2023-01-20 14:41:28 +0100

    motor rotates each 60 degrees equivalent to 900 steps meaning 6 bottles

--- cocktailbar/motorControl/motorControl.ino
+++ cocktailbar/motorControl/motorControl.ino
@@ -1,71 +1,50 @@
-/*
-  Stepper Motor Demonstration 4
-  Stepper-Demo4.ino
-  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
- 
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
-*/
- 
-// Define Constants
- 
-// Connections to A4988
+#define STEPS_PER_REV 360
+#define STEPS_TO_DELAY 900
 
-// Motor steps per rotation
-const int STEPS_PER_REV = 360;
 const int dirPin = 2;  // Direction
 const int stepPin = 3; // Step
 const int startPoint = 0;
-const int bottles = 6;
-int bottlePositionInDegrees = (STEPS_PER_REV / bottles); 
-int stepPosition;
 
+// testing
+int counterInsideLoop;
 
- 
+void generatePulse(int pulse){
+  digitalWrite(stepPin,HIGH); 
+  delayMicroseconds(pulse); 
+  digitalWrite(stepPin,LOW); 
+  delayMicroseconds(pulse);
+}
+
 void setup() {
   Serial.begin(9600);
   // Setup the pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
 }
+
 void loop() {
-  int fullRotate = 512;
-  int degSixty = 85;
-  // makeSteps(degSixty);
-  rotateClockwize();
-  // Set motor direction clockwise
+  //Set the motors to HIGH in order for them to rotate clockwize
   digitalWrite(dirPin,HIGH); 
-  
-  Serial.println(stepPosition);
-  
+  // Function that handles rotating in a specific degree
+  rotateClockwizeAndStopEach60Degrees();
+
 }
 
-void rotateClockwize(){
-    // Spin motor one rotation slowly ()
-    for(int x = 0; x < (STEPS_PER_REV); x++) {
-      // Creates a pulse that drives the STEP pin on the module.
-      digitalWrite(stepPin,HIGH); 
-      generatePulse();
-      if(x == bottlePositionInDegrees){
-        delay(1000);
-      }
-    } 
+void rotateClockwizeAndStopEach60Degrees(){ 
+
+  for(int x = 0; x < (STEPS_PER_REV); x++) {
+    // Creates a pulse that drives the STEP pin on the module.
+    generatePulse(1000);
+    counterInsideLoop++;
+    if(counterInsideLoop % STEPS_TO_DELAY == 0){
+      delay(1000);
+    }
+  }
+  Serial.println(counterInsideLoop);
+
 }
 
-// void makeSteps(int steps){
-//    for(int y = 0; y <= steps; y ++){
-//       digitalWrite(stepPin,HIGH); 
-//       delayMicroseconds(500); 
-//       digitalWrite(stepPin,LOW); 
-//       delayMicroseconds(500);
-//    }
-// }
 
-void generatePulse(int pulse){
-  delayMicroseconds(pulse); 
-  digitalWrite(stepPin,LOW); 
-  delayMicroseconds(pulse);
-}
 
 
 
