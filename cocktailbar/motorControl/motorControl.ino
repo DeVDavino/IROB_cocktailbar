ee6fccd12a49a59b21be62937d30bc159963fb06
DeVDavino <doubled.rosaria@gmail.com>
2023-01-21 08:47:24 +0100

    Added functions for different drinks

--- cocktailbar/motorControl/motorControl.ino
+++ cocktailbar/motorControl/motorControl.ino
@@ -1,5 +1,9 @@
 #define STEPS_PER_REV 360
-#define STEPS_TO_DELAY 900
+#define STEPS_TO_DELAY 200
+#define BACO 900
+#define MALIBUCOLA 1800
+#define RUMFATNA 2700
+#define MALIBUFANTA 3600
 
 const int dirPin = 2;  // Direction
 const int stepPin = 3; // Step
@@ -43,6 +47,53 @@ void rotateClockwizeAndStopEach60Degrees(){
   Serial.println(counterInsideLoop);
 
 }
+
+//rotates motors to first position which is Bacardi Cola
+void rotateToBaco(){
+  for(int x = 0; x < (STEPS_PER_REV); x++){
+    generatePulse(1000);
+    if(counterInsideLoop % BACO == 0){
+      break;
+    }
+  }
+  Serial.println("Bacardi Cola positie is bereikt");
+}
+
+//rotates motors to first position which is Malibu Cola
+void rotateToMalibuCola(){
+  for(int x = 0; x < (STEPS_PER_REV); x++){
+    generatePulse(1000);
+    if(counterInsideLoop % MALIBUCOLA == 0){
+      break;
+    }
+  }
+  Serial.println("Malibu Cola positie is bereikt");
+}
+
+//rotates motors to first position which is Rum Fanta
+void rotateToRumFanta(){
+  for(int x = 0; x < (STEPS_PER_REV); x++){
+    generatePulse(1000);
+    if(counterInsideLoop % RUMFATNA == 0){
+      break;
+    }
+  }
+  Serial.println("Rum Fanta positie is bereikt");
+}
+
+//rotates motors to first position which is Malibu Fanta
+void rotateToMalibuFanta(){
+  for(int x = 0; x < (STEPS_PER_REV); x++){
+    generatePulse(1000);
+    if(counterInsideLoop % MALIBUFANTA == 0){
+      break;
+    }
+  }
+  Serial.println("Malibu Fanta positie is bereikt");
+}
+
+
+
 
 
 
