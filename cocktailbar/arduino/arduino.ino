a07804e8581788f03b574a31f20565c8d634dad4
Nils <97680412+NGHRD@users.noreply.github.com>
2023-01-25 16:04:45 +0100

    Add files via upload

--- cocktailbar/arduino/arduino.ino
+++ cocktailbar/arduino/arduino.ino
@@ -6,8 +6,9 @@
 #define STEPS_PER_REV 360
 #define STEPS_TO_DELAY 200
 #define CLOCKWIZEROTATION HIGH
-#define PUMP_RELAIS_PIN 11
-#define MOTOR_RELAIS_PIN 12
+#define PUMP_1_RELAIS_PIN 11
+#define PUMP_2_RELAIS_PIN 12
+#define MOTOR_RELAIS_PIN 10
 
 Servo myservo;  
 
@@ -22,15 +23,12 @@ int currentState;
 int receive = 0;
 int pos = 0;  
 
-// 0 baco
-// 1 malibu cola
-// 2 rum fanta
-// 3 malibu fanta
-
 void setup() {
   Serial.begin(9600);
   myservo.attach(9);
-  digitalWrite(dirPin, CLOCKWIZEROTATION); 
+  myservo.write(pos);
+
+  delay(500);
   
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(POSITION_PIN, INPUT);
@@ -41,72 +39,75 @@ void setup() {
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(MOTOR_RELAIS_PIN, OUTPUT);
-  pinMode(PUMP_RELAIS_PIN, OUTPUT);
+  pinMode(PUMP_1_RELAIS_PIN, OUTPUT);
+  pinMode(PUMP_2_RELAIS_PIN, OUTPUT);
 
-  
+  digitalWrite(dirPin, CLOCKWIZEROTATION); 
   digitalWrite(MOTOR_RELAIS_PIN, HIGH);
-  digitalWrite(PUMP_RELAIS_PIN, HIGH);
+  digitalWrite(PUMP_1_RELAIS_PIN, HIGH);
+  digitalWrite(PUMP_2_RELAIS_PIN, HIGH);
 
-  myservo.write(pos);
-  
-  
+  if (!calibrateState){
+    calibrate();
+  }
 
-  // if (!calibrateState){
-  //   calibrate();
-  // }
-
-  // delay(5000);
-
-  // movePump(5);
-
-  moveStep(3);
+  vodkaCola();  
 }
 
 void loop() {
-  // Serial.println(calibrateState);
-
   currentState = digitalRead(BUTTON_PIN);
+
+  if (!calibrateState){
+    calibrate();
+  }
 
   // Handeld het verzochte drankje van de esp af
   if(Serial.available()){
     receive = Serial.read();
     switch(receive) {
       case 0:
-        Serial.println("Baco");
-        movePump(1); 
+        Serial.println("Water");
+        water();
         break;
       case 1:
-        Serial.println("Malibu Cola");
-        movePump(20); 
+        Serial.println("Vodka Cola");
+        vodkaCola();
         break;
       case 2:
-        Serial.println("Rum Fanta");
-        movePump(20); 
+        Serial.println("Blue moon");
+        blueMoon(); 
         break;
       case 3:
-        Serial.println("Malibu Fanta");
-        movePump(20); 
+        Serial.println("Chardonnay");
+        chardonnay();
+        break;
+      case 4:
+        Serial.println("Rosé");
+        rose();
+        break;
+      case 5:
+      Serial.println("Cola");
+        cola();
+        break;
+      case 6:
+        Serial.println("Clean");
+        clean();
         break;
     }
+    Serial.write(1);
   }   
 
   // TIJDELIJK OF ALS RESET KNOP
   // Reset button -> volgend drankje kan gemaakt worden.
   if(lastState == LOW && currentState == HIGH){
-    Serial.write(1);   
+    clean();   
   }
 
   lastState = currentState;
-
-  // if(analogRead(POSITION_PIN) > 5) {
-  //   Serial.println("magneetje");
-  // }
-
-  // Serial.println(analogRead(POSITION_PIN));
 }
 
  
-void movePump(int amount) {
+void movePressPump(int amount) {
   for(int i = 0; i < amount; i++){
      for (pos = 0; pos <= 160; pos += 1) { 
     myservo.write(pos);              
@@ -127,6 +128,19 @@ void movePump(int amount) {
 }
 
 
+void colaPump(int time){
+  digitalWrite(PUMP_1_RELAIS_PIN, LOW);
+  delay(time);
+  digitalWrite(PUMP_1_RELAIS_PIN, HIGH);
+}
+
+void spritePump(int time){
+  digitalWrite(PUMP_1_RELAIS_PIN, LOW);
+  delay(time);
+  digitalWrite(PUMP_1_RELAIS_PIN, HIGH);
+}
+
+
 void generatePulse(int pulse){
   digitalWrite(stepPin,HIGH); 
   delayMicroseconds(pulse); 
@@ -134,7 +148,7 @@ void generatePulse(int pulse){
   delayMicroseconds(pulse);
 }
 
-
+ 
 void calibrate(){
   Serial.println("calibrate");
 
@@ -142,7 +156,7 @@ void calibrate(){
   do{
     for(int x = 0; x < (STEPS_PER_REV); x++) {
       generatePulse(1000);
-      if(analogRead(CALIBRATE_PIN) < 10){
+      if(analogRead(CALIBRATE_PIN) < 100){
           calibrateState = true;
           break;
         }
@@ -152,13 +166,6 @@ void calibrate(){
   digitalWrite(MOTOR_RELAIS_PIN, HIGH);
   Serial.write(1);
 }
-
-
-// x Zetje geven plus delay (delay later verwijderen)
-// x Blijven draaien
-// x Magneten herkennen
-// x Stoppen wanneer magneet herkent wordt
-// x Amount forloep toepasen
 
 
 // Amount saat voor aantal posities opschuiven vanaf het calibratie punt.
@@ -176,7 +183,7 @@ void moveStep(int amount) {
     Serial.println("Zetje");
 
     // Geeft een zetje
-    for(int x = 0; x < STEPS_PER_REV; x++) {
+    for(int x = 0; x < 200; x++) {
       generatePulse(1000);
     } 
 
@@ -187,7 +194,7 @@ void moveStep(int amount) {
     // Blijft draaien tot dat er een magneet gedetecteerd wordt
     do{
       for(int x = 0; x < STEPS_PER_REV; x++) {
-        if(analogRead(POSITION_PIN) < 100){
+        if(analogRead(POSITION_PIN) < 200){
           moveMotor = false;
           break;
         }
@@ -200,6 +207,56 @@ void moveStep(int amount) {
 
   Serial.println("Done");
   digitalWrite(MOTOR_RELAIS_PIN, HIGH);
+}
+
+void water(){
+  movePressPump(20);
+}
+
+void vodkaCola(){
+  moveStep(1);
+  delay(1000);
+  movePressPump(10);
+  delay(1000);
+  colaPump(10000);
+  calibrateState = false;
+}
+
+void blueMoon(){
+  moveStep(4);
+  delay(1000);
+  movePressPump(10);
+  delay(1000);
+  // Sprite pump
+  // spritePump(10000);
+  calibrateState = false;
+}
+
+void chardonnay(){
+  moveStep(2);
+  delay(1000);
+  movePressPump(20);
+  calibrateState = false;
+}
+
+void rose(){
+  moveStep(5);
+  delay(1000);
+  movePressPump(20);
+  calibrateState = false;
+}
+
+void cola(){
+  colaPump(10000);
+}
+
+
+void clean() {
+  Serial.println("clean");
+  for(int i = 0; i < 5; i++){
+    movePressPump(12);
+  moveStep(1);
+  }
 }
 
 // Rood - blauw
